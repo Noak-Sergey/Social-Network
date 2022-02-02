5bf507714f317967e909ead96f48ab7e5e6acd6d
Noak-Sergey <noakstuff@gmail.com>
2022-02-02 16:07:05 +0300

    lesson 94, added React.lazy and React.Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,32 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import { Route, RouteComponentProps, withRouter } from 'react-router-dom';
-import { DialogsContainer } from "./components/Dialogs/DialogsContainer";
+//import { DialogsContainer } from "./components/Dialogs/DialogsContainer";
 import { UsersContainer } from "./components/Users/UsersContainer";
-import { ProfileContainer } from "./components/Profile/ProfileContainer";
+//import { ProfileContainer } from "./components/Profile/ProfileContainer";
 import { HeaderContainer } from "./components/Header/HeaderContainer";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { initializeApp } from "./Redux/app-reducer";
 import { AppStateType } from "./Redux/redux-store";
 import { Preloader } from "./components/common/Preloader/Preloader";
-import { LoginContainer } from "./components/Login/LogIn";
+//import { LoginContainer } from "./components/Login/LogIn";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./Redux/redux-store";
+import { Provider } from "react-redux";
+import { store } from "./Redux/redux-store";
+import { withSuspense } from './hoc/withSuspense';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer')
+    .then(({ DialogsContainer }) => ({ default: DialogsContainer }))
+);
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer')
+    .then(({ ProfileContainer }) => ({ default: ProfileContainer }))
+);
+
+const LoginContainer = React.lazy(() => import('./components/Login/LogIn')
+    .then(({ LoginContainer }) => ({ default: LoginContainer }))
+);
 
 type MapStateToPropsType = {
     initialized: boolean
@@ -46,13 +59,17 @@ class App extends React.Component<AppType> {
                 <Navbar />
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                        render={() => <DialogsContainer />} />
+                        render={() => {
+                            return <React.Suspense fallback={<Preloader/>}>
+                                <DialogsContainer />
+                            </React.Suspense>
+                        }} />
                     <Route path='/profile/:userId?'
-                        render={() => <ProfileContainer />} />
+                        render={withSuspense(ProfileContainer)} />
                     <Route path='/users'
                         render={() => <UsersContainer />} />
                     <Route path='/login'
-                        render={() => <LoginContainer />} />
+                        render={withSuspense(LoginContainer)} />
                 </div>
             </div>
 
